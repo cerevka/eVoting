c1cf27ab55f63e5e21f3107f4e31f742bf6ae7be
Tomáš Čerevka <tomas.cerevka@gmail.com>
2011-05-20 10:46:28 +0200

    Upravena sprava uzivatele.

--- eVotingWeb/src/java/evoting/sessionScoped/UserManagedBean.java
+++ eVotingWeb/src/java/evoting/sessionScoped/UserManagedBean.java
@@ -1,10 +1,14 @@
 package evoting.sessionScoped;
 
+import java.io.Serializable;
+import java.security.Principal;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -12,7 +16,7 @@ import javax.servlet.http.HttpServletRequest;
  */
 @ManagedBean(name = "user")
 @SessionScoped
-public class UserManagedBean {
+public class UserManagedBean implements Serializable {
 
     private String login;
 
@@ -36,8 +40,9 @@ public class UserManagedBean {
     public String getPassword() {
         return this.password;
     }
-    
+
     public String doLogin() {
+        doLogout();
         HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
         try {
             request.login(this.login, this.password);
@@ -46,6 +51,53 @@ public class UserManagedBean {
         } catch (ServletException ex) {
             this.login = this.password = null;
             return "fail";
-        }        
+        }
+    }
+
+    public String doLogout() {
+        HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true);
+        session.invalidate();
+        return "logout";
+    }
+
+    public String getRole() {
+        Principal principal = FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal();
+        if (principal == null) {
+            return null;
+        }
+        return principal.getName();
+    }
+
+    public boolean isVoter() {
+        ExternalContext ectx = FacesContext.getCurrentInstance().getExternalContext();
+        if (ectx.isUserInRole("voters")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isAdministrator() {
+        ExternalContext ectx = FacesContext.getCurrentInstance().getExternalContext();
+        if (ectx.isUserInRole("administrators")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isCommissioner() {
+        ExternalContext ecxt = FacesContext.getCurrentInstance().getExternalContext();
+        if (ecxt.isUserInRole("commissioners")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String getUserName() {
+        ExternalContext ectx = FacesContext.getCurrentInstance().getExternalContext();
+        return ectx.getRemoteUser();
+
     }
 }
